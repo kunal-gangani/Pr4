5e4669eee2430edcd601df37e30dbbdc04e34250
kunal-gangani <150250846+kunal-gangani@users.noreply.github.com>
2023-12-08 14:16:50 +0530

    Update Pr4_7.c

--- Pr4_7.c
+++ Pr4_7.c
@@ -1,3 +1,11 @@
+/*
+Q.7 Write a Program to print the below pattern using nested for loop.
+           *
+        *     *
+      *         *
+    *             *
+  *                 *
+*/
 #include <stdio.h>
 void main() {
   int i,j,k;
